d4256d028f35065398520cfb7e8dd2ecaa07c733
Christophe Ninucci <christophe.ninucci@direccte.gouv.fr>
2022-05-17 07:28:24 +0200

    add command switch keycloak

--- main.go
+++ main.go
@@ -1,11 +1,35 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/signaux-faibles/keycloakUpdater/v2/config"
 	"github.com/signaux-faibles/keycloakUpdater/v2/logger"
 )
 
 func main() {
+	if len(os.Args) == 1 {
+		keycloak()
+		wekan()
+	} else if len(os.Args) == 2 {
+		if os.Args[1] == "keycloak" {
+			keycloak()
+		} else if os.Args[1] == "wekan" {
+			wekan()
+		} else {
+			usage()
+		}
+	} else {
+		usage()
+	}
+}
+
+func usage() {
+	fmt.Println("usage: keycloakUpdater [keycloak|wekan]")
+}
+
+func keycloak() {
 	conf, err := config.InitConfig("/workspace/config.toml")
 	if err != nil {
 		panic(err)
@@ -21,4 +45,8 @@ func main() {
 	if err = UpdateAll(&kc, clientId, conf.Realm, conf.Clients, conf.Stock.UsersAndRolesFilename, conf.Access.Username); err != nil {
 		panic(err)
 	}
+}
+
+func wekan() {
+	fmt.Println("Wekan boilerplate not (yet) implemented")
 }
