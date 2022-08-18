1099e209f0029362ba83724cf587b16d521046b0
Raphaël Squelbut <raphael.squelbut@alibeez.com>
2022-08-18 14:04:34 +0200

    fix: ajoute des tests dans le chargement des users

--- user.go
+++ user.go
@@ -1,191 +1,193 @@
 package main
 
 import (
-	"errors"
-	"github.com/signaux-faibles/keycloakUpdater/v2/logger"
-	"gopkg.in/yaml.v3"
-	"os"
-	"sort"
-	"strings"
+  errors "github.com/pkg/errors"
+  "github.com/signaux-faibles/keycloakUpdater/v2/logger"
+  "gopkg.in/yaml.v3"
+  "os"
+  "sort"
+  "strings"
 
-	"github.com/Nerzal/gocloak/v11"
+  "github.com/Nerzal/gocloak/v11"
 )
 
 // User is the definition of an user in excel state
 type User struct {
-	Niveau            string
-	Email             string
-	Prenom            string
-	Nom               string
-	Segment           string
-	Fonction          string
-	Employeur         string
-	Goup              string
-	Scope             []string
-	AccesGeographique string `yaml:"accesGeographique"`
+  Niveau            string
+  Email             string
+  Prenom            string
+  Nom               string
+  Segment           string
+  Fonction          string
+  Employeur         string
+  Goup              string
+  Scope             []string
+  AccesGeographique string `yaml:"accesGeographique"`
 }
 
 // Users is the collection of wanted users
 type Users map[string]User
 
 func (user User) roles() Roles {
-	var roles Roles
-	if user.Niveau == "a" {
-		roles = append(roles, "urssaf", "dgefp", "bdf")
-	}
-	if user.Niveau == "b" {
-		roles = append(roles, "dgefp")
-	}
-	if user.Niveau != "0" {
-		roles = append(roles, "score", "detection", "pge")
-		if user.AccesGeographique != "" {
-			roles = append(roles, user.AccesGeographique)
-		}
-		if !(len(user.Scope) == 1 && user.Scope[0] == "") {
-			roles = append(roles, user.Scope...)
-		}
-	}
-	return roles
+  var roles Roles
+  if user.Niveau == "a" {
+    roles = append(roles, "urssaf", "dgefp", "bdf")
+  }
+  if user.Niveau == "b" {
+    roles = append(roles, "dgefp")
+  }
+  if user.Niveau != "0" {
+    roles = append(roles, "score", "detection", "pge")
+    if user.AccesGeographique != "" {
+      roles = append(roles, user.AccesGeographique)
+    }
+    if !(len(user.Scope) == 1 && user.Scope[0] == "") {
+      roles = append(roles, user.Scope...)
+    }
+  }
+  return roles
 }
 
 func (users Users) addUser(input User) Users {
-	user := input.format()
-	users[user.Email] = user
-	return users
+  user := input.format()
+  users[user.Email] = user
+  return users
 }
 
 // GetUser resolves existing user from its username
 func (kc KeycloakContext) GetUser(username string) (gocloak.User, error) {
-	for _, u := range kc.Users {
-		if u != nil && u.Username != nil && strings.EqualFold(*u.Username, username) {
-			return *u, nil
-		}
-	}
-	return gocloak.User{}, errors.New("user does not exists")
+  for _, u := range kc.Users {
+    if u != nil && u.Username != nil && strings.EqualFold(*u.Username, username) {
+      return *u, nil
+    }
+  }
+  return gocloak.User{}, errors.New("user does not exists")
 }
 
 // Compare returns missing, obsoletes, disabled users from kc.Users from []user
 func (users Users) Compare(kc KeycloakContext) ([]gocloak.User, []gocloak.User, []gocloak.User, []gocloak.User) {
-	var missing []User
-	var enable []gocloak.User
-	var obsolete []gocloak.User
-	var current []gocloak.User
+  var missing []User
+  var enable []gocloak.User
+  var obsolete []gocloak.User
+  var current []gocloak.User
 
-	for _, u := range users {
-		kcu, err := kc.GetUser(u.Email)
-		if err != nil {
-			missing = append(missing, u)
-		}
-		if err == nil && !*kcu.Enabled {
-			enable = append(enable, kcu)
-		}
-	}
+  for _, u := range users {
+    kcu, err := kc.GetUser(u.Email)
+    if err != nil {
+      missing = append(missing, u)
+    }
+    if err == nil && !*kcu.Enabled {
+      enable = append(enable, kcu)
+    }
+  }
 
-	for _, kcu := range kc.Users {
-		if _, ok := users[strings.ToLower(*kcu.Username)]; !ok {
-			if *kcu.Enabled {
-				obsolete = append(obsolete, *kcu)
-			}
-		} else {
-			current = append(current, *kcu)
-		}
-	}
-	return toGocloakUsers(missing), obsolete, enable, current
+  for _, kcu := range kc.Users {
+    if _, ok := users[strings.ToLower(*kcu.Username)]; !ok {
+      if *kcu.Enabled {
+        obsolete = append(obsolete, *kcu)
+      }
+    } else {
+      current = append(current, *kcu)
+    }
+  }
+  return toGocloakUsers(missing), obsolete, enable, current
 }
 
 func toGocloakUsers(users []User) []gocloak.User {
-	var u []gocloak.User
-	for _, user := range users {
-		u = append(u, user.ToGocloakUser())
-	}
-	return u
+  var u []gocloak.User
+  for _, user := range users {
+    u = append(u, user.ToGocloakUser())
+  }
+  return u
 }
 
 // ToGocloakUser creates a new gocloak.User object from User specification
 func (user User) ToGocloakUser() gocloak.User {
-	t := true
-	attributes := make(map[string][]string)
-	if user.Goup != "" {
-		attributes["goup_path"] = []string{user.Goup}
-	}
-	attributes["fonction"] = []string{user.Fonction}
-	attributes["employeur"] = []string{user.Employeur}
+  t := true
+  attributes := make(map[string][]string)
+  if user.Goup != "" {
+    attributes["goup_path"] = []string{user.Goup}
+  }
+  attributes["fonction"] = []string{user.Fonction}
+  attributes["employeur"] = []string{user.Employeur}
 
-	if user.Segment != "" {
-		attributes["segment"] = []string{user.Segment}
-	}
-	return gocloak.User{
-		Username:      &user.Email,
-		Email:         &user.Email,
-		EmailVerified: &t,
-		FirstName:     &user.Prenom,
-		LastName:      &user.Nom,
-		Enabled:       &t,
-		Attributes:    &attributes,
-	}
+  if user.Segment != "" {
+    attributes["segment"] = []string{user.Segment}
+  }
+  return gocloak.User{
+    Username:      &user.Email,
+    Email:         &user.Email,
+    EmailVerified: &t,
+    FirstName:     &user.Prenom,
+    LastName:      &user.Nom,
+    Enabled:       &t,
+    Attributes:    &attributes,
+  }
 }
 
 func compareAttributes(a *map[string][]string, b *map[string][]string) bool {
-	if a == nil && b == nil {
-		return true
-	}
-	if a == nil || b == nil {
-		return false
-	}
-	for k := range *b {
-		if _, ok := (*a)[k]; !ok {
-			return false
-		}
-	}
-	for k, attribA := range *a {
-		attribB, ok := (*b)[k]
-		if !ok {
-			return false
-		}
-		sort.Strings(attribB)
-		sort.Strings(attribA)
-		if strings.Join(attribA, "\t") != strings.Join(attribB, "\t") {
-			return false
-		}
-	}
-	return true
+  if a == nil && b == nil {
+    return true
+  }
+  if a == nil || b == nil {
+    return false
+  }
+  for k := range *b {
+    if _, ok := (*a)[k]; !ok {
+      return false
+    }
+  }
+  for k, attribA := range *a {
+    attribB, ok := (*b)[k]
+    if !ok {
+      return false
+    }
+    sort.Strings(attribB)
+    sort.Strings(attribA)
+    if strings.Join(attribA, "\t") != strings.Join(attribB, "\t") {
+      return false
+    }
+  }
+  return true
 }
 
 func (user User) format() User {
-	r := User{
-		Niveau:            strings.ToLower(user.Niveau),
-		Email:             strings.Trim(strings.ToLower(user.Email), " "),
-		Nom:               strings.ToUpper(user.Nom),
-		Prenom:            strings.ToTitle(user.Prenom),
-		Segment:           strings.ToUpper(user.Segment),
-		Fonction:          user.Fonction,
-		Employeur:         strings.ToUpper(user.Employeur),
-		Goup:              user.Goup,
-		Scope:             user.Scope,
-		AccesGeographique: user.AccesGeographique,
-	}
-	return r
+  r := User{
+    Niveau:            strings.ToLower(user.Niveau),
+    Email:             strings.Trim(strings.ToLower(user.Email), " "),
+    Nom:               strings.ToUpper(user.Nom),
+    Prenom:            strings.ToTitle(user.Prenom),
+    Segment:           strings.ToUpper(user.Segment),
+    Fonction:          user.Fonction,
+    Employeur:         strings.ToUpper(user.Employeur),
+    Goup:              user.Goup,
+    Scope:             user.Scope,
+    AccesGeographique: user.AccesGeographique,
+  }
+  return r
 }
 
 func loadUsers(usersFilename []string) (Users, error) {
-	users := make(Users)
-	fields := logger.DataForMethod("loadUsers")
-	if len(usersFilename) <= 0 {
-		logger.Error("pas de fiche utilisateur", fields)
-		return nil, errors.New("pas de fiche utilisateur à charger")
-	}
-	for _, f := range usersFilename {
-		fields.AddAny("file", f)
-		logger.Debug("charge la fiche", fields)
-		current := User{}
-		data, err := os.ReadFile(f)
-		if err != nil {
-			return nil, err
-		}
-		if err = yaml.Unmarshal(data, &current); err != nil {
-			return nil, err
-		}
-		users.addUser(current)
-	}
-	return users, nil
+  users := make(Users)
+  fields := logger.DataForMethod("loadUsers")
+  if len(usersFilename) <= 0 {
+    return nil, errors.New("pas de fiche utilisateur à charger")
+  }
+  for _, f := range usersFilename {
+    fields.AddAny("file", f)
+    logger.Debug("charge la fiche", fields)
+    current := User{}
+    data, err := os.ReadFile(f)
+    if err != nil {
+      return nil, errors.WithMessagef(err, "problème avec la fiche utilisateur %s", f)
+    }
+    if err = yaml.Unmarshal(data, &current); err != nil {
+      return nil, errors.WithMessagef(err, "problème avec le contenu de la fiche utilisateur %s", f)
+    }
+    users.addUser(current)
+  }
+  if len(users) <= 0 {
+    return nil, errors.New("aucune fiche utilisateur chargée")
+  }
+  return users, nil
 }
--- user_test.go
+++ user_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/stretchr/testify/assert"
+	"io/fs"
 	"testing"
 )
 
@@ -14,9 +15,8 @@ func Test_loadUsers(t *testing.T) {
 		"test/sample/users/un_mec_pas_de_l_urssaf.yml",
 	}
 	users, err := loadUsers(filenames)
-	if err != nil {
-		t.Fatalf(err.Error())
-	}
+
+	assertions.Nil(err)
 	assertions.Len(users, 3)
 	assertions.NotNil(users["john.doe@zone51.gov.fr"])
 	assertions.NotNil(users["ti_admin"])
@@ -29,4 +29,46 @@ func Test_loadUsers(t *testing.T) {
 	assertions.NotNil(raphael)
 	assertions.Contains(raphael.roles(), "France entière")
 	assertions.Contains(raphael.roles(), "urssaf")
+}
+
+func Test_loadNoUsers(t *testing.T) {
+	assertions := assert.New(t)
+	t.Log("test de chargement avec aucun users")
+	var filenames []string
+	users, err := loadUsers(filenames)
+
+	assertions.Nil(users)
+	assertions.NotNil(err)
+	assertions.EqualError(err, "pas de fiche utilisateur à charger")
+}
+
+func Test_loadUsers_noSuchFile(t *testing.T) {
+	assertions := assert.New(t)
+	t.Log("test de chargement des users")
+	fichierInexistant := "test/sample/users/fichier_inexistant.yml"
+	filenames := []string{
+		fichierInexistant,
+	}
+	users, err := loadUsers(filenames)
+
+	assertions.Nil(users)
+	assertions.NotNil(err)
+	assertions.Error(err)
+	assertions.ErrorAs(err, &fs.ErrNotExist)
+}
+
+func Test_loadUsers_nonYamlFile(t *testing.T) {
+	assertions := assert.New(t)
+	t.Log("test de chargement des users")
+	invalidYamlFile := "test/sample/users/invalid_yml_file.yml"
+	filenames := []string{
+		"test/sample/users/admin.yml",
+		invalidYamlFile,
+	}
+	users, err := loadUsers(filenames)
+	expectedMessage := "problème avec le contenu de la fiche utilisateur " + invalidYamlFile + ": yaml: mapping values are not allowed in this context"
+	assertions.Nil(users)
+	assertions.NotNil(err)
+	assertions.Error(err)
+	assertions.EqualError(err, expectedMessage)
 }
