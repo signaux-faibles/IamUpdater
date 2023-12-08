fdff9afcc8416a6273fb425e395e5dc52140c79b
Raphaël Squelbut <raphael.squelbut@shodo.io>
2023-12-08 18:44:45 +0100

    on met les logs en TRACE pour la CI

--- pkg/logger/levels_test.go
+++ pkg/logger/levels_test.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"keycloakUpdater/v2/pkg/structs"
 )
@@ -51,4 +52,24 @@ func Test_level_Notice(t *testing.T) {
 	ass.NotContains(string(logsFromFile), "level="+slog.LevelInfo.String())
 	ass.Contains(string(logsFromFile), "level="+levelNoticeName)
 	ass.Contains(string(logsFromFile), "level="+slog.LevelWarn.String())
+}
+
+func Test_parseLogLevel(t *testing.T) {
+	type args struct {
+		logLevel string
+	}
+	tests := []struct {
+		name string
+		args args
+		want slog.Leveler
+	}{
+		{"parse niveau TRACE", args{"TRACE"}, levelTrace},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := parseLogLevel(tt.args.logLevel)
+			require.NoError(t, err)
+			assert.Equalf(t, tt.want, got, "parseLogLevel(%v)", tt.args.logLevel)
+		})
+	}
 }
